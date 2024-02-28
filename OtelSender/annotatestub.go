712cb95874b7c3578fdf4f9bbaaa51d6175d22f9
Hua Liang <huliang@microsoft.com>
2024-02-28 23:04:49 +0000

    Rewrite the stub service and add readme

---
+++ OtelSender/annotatestub.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"io"
+	"log"
+	"math/rand"
+	"net/http"
+	"strconv"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/metric"
+)
+
+var (
+	tracer  = otel.Tracer("annotate")
+	meter   = otel.Meter("annotate")
+	annotateCnt metric.Int64Counter
+)
+
+func init() {
+	var err error
+	annotateCnt, err = meter.Int64Counter("annotate",
+		metric.WithDescription("The number of annotate call."),
+		metric.WithUnit("{annotate}"))
+	if err != nil {
+		panic(err)
+	}
+}
+
+func annotate(w http.ResponseWriter, r *http.Request) {
+	// Read the request body  
+	body, err := io.ReadAll(r.Body)  
+	if err != nil {  
+		http.Error(w, "Failed to read request body", http.StatusInternalServerError)  
+		return  
+	}  
+	
+	// Get the request body  
+	bodyStr := string(body)
+
+	ctx, span := tracer.Start(r.Context(), "annotate")
+	defer span.End()
+
+	// random annotate result
+	annotateResult := bodyStr + ": with random number " + strconv.Itoa(rand.Intn(100))
+
+	// Add the custom attribute to the span and counter.
+	annotateResultValueAttr := attribute.String("annotate.result.value", annotateResult)
+	span.SetAttributes(annotateResultValueAttr)
+	annotateCnt.Add(ctx, 1, metric.WithAttributes(annotateResultValueAttr))
+
+	resp := annotateResult + "\n"
+	if _, err := io.WriteString(w, resp); err != nil {
+		log.Printf("Write failed: %v\n", err)
+	}
+}
+
+// liveness endpoint just returns 200
+func liveness(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("OK"))
+}
+
+// readiness endpoint just returns 200
+func readiness(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("OK"))
+}
--- OtelSender/otel.go
+++ OtelSender/otel.go
@@ -6,17 +6,52 @@ import (
 	"time"
 
 	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/exporters/stdout/stdoutmetric"
 	"go.opentelemetry.io/otel/propagation"
 	"go.opentelemetry.io/otel/sdk/metric"
+	"go.opentelemetry.io/otel/sdk/resource"
 	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 
+	"go.opentelemetry.io/otel/exporters/otlp/otlpmetric/otlpmetrichttp"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp"
 )
 
-func newExporter(ctx context.Context) (trace.SpanExporter, error) {
-	//return otlptracehttp.New(ctx, otlptracehttp.WithHeaders(map[string]string{"123": "456"}))
-	return otlptracehttp.New(ctx)
+var AZURE_TENANT_ID = "33e01921-4d64-4f8c-a055-5bdaffd5e33d"
+
+var AZURE_CLIENT_ID = "9c7ae59d-9323-4423-a0da-38ddce774875"
+
+// var AZURE_CLIENT_SECRET Cannot check in secret to Github repo
+
+// The public otel collector scope.
+var SCOPE = "fa65c9d1-e75e-4ac1-b7c1-608189fd7969/.default"
+
+// The RAIUAI identity ID in AME
+var UAI_CLIENT_ID = "1baa67a6-59c1-4c0f-a675-ee2682793b42"
+
+
+
+func newTraceExporter(ctx context.Context) (trace.SpanExporter, error) {
+	token, err := getToken(ctx, SCOPE, UAI_CLIENT_ID)
+	if err != nil {
+		return nil, err
+	}
+
+	kv := make(map[string]string)
+	kv["Authorization"] = "Bearer " + token
+
+	return otlptracehttp.New(ctx, otlptracehttp.WithHeaders(kv))
+}
+
+func newMetricsExporter(ctx context.Context) (metric.Exporter, error) {
+	token, err := getToken(ctx, SCOPE, UAI_CLIENT_ID)
+	if err != nil {
+		return nil, err
+	}
+
+	kv := make(map[string]string)
+	kv["Authorization"] = "Bearer " + token
+
+	return otlpmetrichttp.New(ctx, otlpmetrichttp.WithHeaders(kv))
 }
 
 // setupOTelSDK bootstraps the OpenTelemetry pipeline.
@@ -55,13 +90,13 @@ func setupOTelSDK(ctx context.Context) (shutdown func(context.Context) error, er
 	otel.SetTracerProvider(tracerProvider)
 
 	// Set up meter provider.
-	//meterProvider, err := newMeterProvider()
-	//if err != nil {
-	//	handleErr(err)
-	//	return
-	//}
-	//shutdownFuncs = append(shutdownFuncs, meterProvider.Shutdown)
-	//otel.SetMeterProvider(meterProvider)
+	meterProvider, err := newMeterProvider()
+	if err != nil {
+		handleErr(err)
+		return
+	}
+	shutdownFuncs = append(shutdownFuncs, meterProvider.Shutdown)
+	otel.SetMeterProvider(meterProvider)
 
 	return
 }
@@ -76,20 +111,15 @@ func newPropagator() propagation.TextMapPropagator {
 func newTraceProvider() (*trace.TracerProvider, error) {
 	//traceExporter, err := stdouttrace.New(
 	//	stdouttrace.WithPrettyPrint())
-	//res := resource.NewWithAttributes(
-	//	semconv.SchemaURL,
-	//	semconv.ServiceNameKey.String("myService"),
-	//	semconv.ServiceNamespaceKey.String("myNamespace"),
-	//	semconv.ServiceVersionKey.String("1.0.0"),
-	//	semconv.ServiceInstanceIDKey.String("abcdef12345"),
-	//)
+	res := resource.NewWithAttributes(
+		semconv.SchemaURL,
+		semconv.ServiceNameKey.String("StubRAIService"),
+		semconv.ServiceNamespaceKey.String("StubRAIOTrace"),
+		semconv.ServiceVersionKey.String("1.0.0"),
+		semconv.ServiceInstanceIDKey.String("StubRAIOInstance"),
+	)
 
-	//res, err := resource.New(
-	//	context.Background(),
-		//resource.WithFromEnv(),
-	//)
-
-	traceExporter, err := newExporter(context.Background())
+	traceExporter, err := newTraceExporter(context.Background())
 	if err != nil {
 		return nil, err
 	}
@@ -98,14 +128,21 @@ func newTraceProvider() (*trace.TracerProvider, error) {
 		trace.WithBatcher(traceExporter,
 			// Default is 5s. Set to 1s for demonstrative purposes.
 			trace.WithBatchTimeout(time.Second)),
-		//trace.WithResource(res),
+		trace.WithResource(res),
 	)
-	println("No resources setting")
 	return traceProvider, nil
 }
 
 func newMeterProvider() (*metric.MeterProvider, error) {
-	metricExporter, err := stdoutmetric.New()
+	res := resource.NewWithAttributes(
+		semconv.SchemaURL,
+		semconv.ServiceNameKey.String("StubRAIService"),
+		semconv.ServiceNamespaceKey.String("StubRAIOMetrics"),
+		semconv.ServiceVersionKey.String("1.0.0"),
+		semconv.ServiceInstanceIDKey.String("StubRAIOInstance"),
+	)
+
+	metricExporter, err := newMetricsExporter(context.Background())
 	if err != nil {
 		return nil, err
 	}
@@ -114,6 +151,7 @@ func newMeterProvider() (*metric.MeterProvider, error) {
 		metric.WithReader(metric.NewPeriodicReader(metricExporter,
 			// Default is 1m. Set to 3s for demonstrative purposes.
 			metric.WithInterval(3*time.Second))),
+		metric.WithResource(res),
 	)
 	return meterProvider, nil
 }
